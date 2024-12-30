3cb3f42da8f230536c633dc73c51a005248581a3
unknown <ramovic225@gmail.com>
2024-12-30 13:47:18 +0100

    tasks styled by color

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="task-container">
+    <div class="task-container" :class="statusClass">
         <div class="status">
-            <div class="dot"></div>
+            <div class="dot" :class="statusClass"></div>
             <span>{{ task.status }}</span>
         </div>
         <div class="title">
@@ -17,7 +17,23 @@
     </div>
 </template>
 <script setup>
-defineProps(['task'])
+
+import { computed } from 'vue';
+
+const props = defineProps(['task'])
+
+const statusClass = (computed(() => {
+    switch (props.task.status) {
+        case 'Completed':
+            return 'status-completed';
+        case 'To Do':
+            return 'status-to-do';
+        case 'In Progress':
+            return 'status-in-progress'
+        default: return ''
+    }
+}))
+
 </script>
 
 <style scoped>
@@ -25,7 +41,6 @@ defineProps(['task'])
     display: flex;
     flex-direction: column;
     padding: 14px;
-    border-top: 3px solid red;
     box-shadow: 4px 2px 4px rgba(0, 0, 0, 0.15);
     min-width: 200px;
     width: fit-content;
@@ -36,7 +51,6 @@ defineProps(['task'])
     align-items: center;
     gap: 6px;
     width: fit-content;
-    border: 1px solid red;
     padding: 2px 10px;
     border-radius: 6px;
 }
@@ -44,7 +58,6 @@ defineProps(['task'])
 .dot {
     width: 5px;
     height: 5px;
-    background: red;
     border-radius: 50%;
 }
 
@@ -61,5 +74,29 @@ defineProps(['task'])
     display: flex;
     align-items: center;
     gap: 6px;
+}
+
+.status-to-do {
+    border-top: 3px solid blue;
+}
+
+.status-to-do .dot {
+    background: blue;
+}
+
+.status-completed {
+    border-top: 3px solid red;
+}
+
+.status-completed .dot {
+    background: red;
+}
+
+.status-in-progress {
+    border-top: 3px solid green;
+}
+
+.status-in-progress .dot {
+    background: green;
 }
 </style>
